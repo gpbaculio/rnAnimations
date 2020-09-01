88698ab6394387b7d2bad6d305932f50d59a5ff4
gpbaculio <gpbaculio@gmail.com>
2020-09-02 01:28:30 +0800

    initialize bottom section icons

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -6,13 +6,16 @@ import TopSectionImg from './TopSectionImg';
 import MainContent from './MainContent';
 
 const topSectionImgSrc = require('../../assets/images/pattern1.png');
+const facebookIcon = require('../../assets/images/icons/icon-facebook.png');
+const appleIcon = require('../../assets/images/icons/icon-apple.png');
+const googleIcon = require('../../assets/images/icons/icon-google.png');
 
 interface ContainerProps {
   topSectionImg: number;
   children: ReactNode;
 }
 
-export const assets = [topSectionImgSrc];
+export const assets = [topSectionImgSrc, facebookIcon, appleIcon, googleIcon];
 
 const Container = ({ topSectionImg, children }: ContainerProps) => {
   const [topSectionWidth, setTopSectionWidth] = useState(0);
@@ -40,10 +43,14 @@ Container.defaultProps = {
 };
 
 export default Container;
-
+const IconsSection = styled.View`
+  flex-direction: row;
+`;
 const BottomSection = styled.View`
   flex: 0.2;
   background-color: black;
+  align-items: center;
+  justify-content: center;
 `;
 
 const StyledViewContainer = styled(View)`
